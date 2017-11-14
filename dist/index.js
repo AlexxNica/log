9f25183905abfb577d77501a42fc5d0b4ba458a6
AlexxNica <kryndex@gmail.com>
2017-11-13 22:21:54 -0200

    update dist files

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,22 @@
 'use strict';
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var log = {
     verboseLevel: 0,
     error: function (message) {
         return this.verboseLevel >= 0 && console.error(message);
     },
     warn: function (message) {
-        return this.verboseLevel >= 0 && console.warn(message);
+        return this.verboseLevel >= 1 && console.warn(message);
     },
     info: function (message) {
-        return this.verboseLevel >= 1 && console.info(message);
+        return this.verboseLevel >= 2 && console.info(message);
     },
     debug: function (message) {
         // @TODO: change from .log to .debug.
-        return this.verboseLevel >= 2 && console.log(message);
+        return this.verboseLevel >= 3 && console.log(message);
     },
     log: function (message) {
-        return this.verboseLevel >= 0 && console.log(message);
-    }
+        return this.verboseLevel >= 4 && console.log(message);
+    },
 };
-exports["default"] = log;
+exports.default = log;
